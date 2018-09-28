63ce7ca5104ba87a91c55e7791f1b094b2c4a330
EnriquePallares <eapallaress@gmail.com>
2018-09-27 19:32:20 -0500

    Interfaz Terminada

---
+++ app/src/main/java/com/example/android/personasmaterial/AgregarPersonas.java
@@ -0,0 +1,13 @@
+package com.example.android.personasmaterial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarPersonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_personas);
+    }
+}
